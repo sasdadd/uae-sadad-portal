3e25878368a615cc7815f5d5f52a2d5345dc579d
Salman Al-Enezi <smsa.moi.sa@gmail.com>
2026-04-26 18:21:26 -0400

    Fix: Simplify App.tsx to isolate and resolve Illegal constructor error

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,5 @@
-import { Toaster } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
 import { Route, Switch } from "wouter";
-import ErrorBoundary from "./components/ErrorBoundary";
-import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 import UserData from "./pages/UserData";
 import SelectBank from "./pages/SelectBank";
@@ -28,14 +24,7 @@ function Router() {
 
 function App() {
   return (
-    <ErrorBoundary>
-      <ThemeProvider defaultTheme="light">
-        <TooltipProvider>
-          <Toaster />
-          <Router />
-        </TooltipProvider>
-      </ThemeProvider>
-    </ErrorBoundary>
+    <Router />
   );
 }
 
